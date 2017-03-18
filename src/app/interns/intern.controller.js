0c575e900f9c731aec9ad26d9fc362e6aac502b1
Alecu Marian <a.marian.alexandru@gmail.com>
2017-03-18 21:59:49 +0200

    Fix data set animation

--- src/app/interns/intern.controller.js
+++ src/app/interns/intern.controller.js
@@ -1,13 +1,16 @@
 (function () {
-  'use strict';
+  "use strict";
 
   angular
-    .module('sweek2017')
-    .controller('InternController', InternController);
+    .module("sweek2017")
+    .controller("InternController", InternController);
 
   /** @ngInject */
   function InternController($scope) {
 
+    var groupsOrder = ["city", "name", "subfiled", "title"];
+    var currentGroup = 0;
+    var visualization = null;
 
     var data = [{
       "name": "City"
@@ -39,29 +42,51 @@
     activate();
 
     function activate() {
-      $.getJSON('data.json', function (json) {
+      $.getJSON("data.json", function (json) {
         sample_data = json;
-        console.debug(sample_data);
         vm.inputChange();
       });
     }
 
     function inputChange() {
 
-      // instantiate d3plus
-      var visualization = d3plus.viz()
-        .container(".bubbles")     // container DIV to hold the visualization
-        .data(sample_data)     // data to use with the visualization
-        .type("bubbles")       // visualization type
-        .id(["name", "city", "subfield", "title"]) // nesting keys
-        .depth(1)              // 0-based depth
-        //.size("value")         // key name to size bubbles
-        .color("name")        // color by each group
-        .draw();             // finally, draw the visualization!
+      drawChart(sample_data, ["city", "name"]);
+    }
 
-      // visualization.on('click', function () {
-      //   console.debug('click');
-      // });
+    function drawChart(data, filters) {
+
+      if (visualization) {
+
+        visualization.data(data).id(filters).draw();
+
+      } else {
+
+        visualization = d3plus.viz()
+          .container(".bubbles")     // container DIV to hold the visualization
+          .data(data)        // data to use with the visualization
+          .type("bubbles")          // visualization type
+          .id(filters)              // nesting keys
+          .depth(1)                 // 0-based depth
+          //.size("value")          // key name to size bubbles
+          .color(filters[0])          // color by each group
+          .mouse({
+            "move": false,                        // key will also take custom function
+            "click": chartClick
+          })
+          .draw();                  // finally, draw the visualization!
+      }
+    }
+
+    function chartClick(elem) {
+
+      var groupIndex = groupsOrder[currentGroup];
+      var groupValue = elem[groupIndex];
+      currentGroup = groupsOrder.indexOf(groupIndex) + 1;
+
+      drawChart(sample_data.filter(function (elem) {
+        return elem[groupIndex] == groupValue;
+      }), [groupsOrder[currentGroup], groupsOrder[currentGroup + 1]]);
+
     }
 
   }
